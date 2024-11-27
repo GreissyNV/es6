0704c4d336b0c953c984ad205ef8b54c9dbbd924
Greissy Naranjo <greissynv@gmail.com>
2024-11-27 10:41:41 +0100

    if

--- 05-condicional/js/app-01.js
+++ 05-condicional/js/app-01.js
@@ -1,2 +1,31 @@
 // Estructuras condicionales
-let haceSol = false;
+let haceSol = true;
+
+if (haceSol) {
+//console.log("Voy a la playa!");
+} else {
+    //console.log("Me quedo en casa");
+}
+
+haceSol = false;
+let haceCalor = true;
+
+if (haceSol || haceCalor) {
+    //console.log("Voy a la playa!");
+} else {
+    //console.log("Me quedo en casa");
+}
+
+haceSol = true;
+tengoHambre = true;
+if (haceSol){
+    console.log("Voy a la playa!");
+} else if(tengoHambre){
+    console.log("Hago palomitas");
+} else {
+    console.log("No se que hacer..");
+}
+
+
+
+console.log("Fin del script");
