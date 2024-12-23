616edca41bdb855e8949c6c8a2591b0cd23f310d
Greissy Naranjo <greissynv@gmail.com>
2024-12-23 10:18:05 +0100

    plantilla

--- DOM/03-selectores-un-elemento/js/script.js
+++ DOM/03-selectores-un-elemento/js/script.js
@@ -13,3 +13,18 @@ itemInput.addEventListener("keyup", () => {
   console.log(itemInput.value);
   tituloPrincipal.innerText = itemInput.value;
 });
+
+
+const btnH1 = document.getElementById("btnH1");
+
+
+btnH1.addEventListener("keyup", () => {
+  console.log(btnH1.value);
+  btnH1.innerText = btnH1.value;
+});
+
+
+
+//Seleccionar el boton 
+//Añadirle un evento o un click
+//Actualizar el contenidodel h1 con el value del imput
