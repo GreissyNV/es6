1889b576838556cbacc3019680d0f2cfacb0c7d6
Greissy Naranjo <greissynv@gmail.com>
2025-01-10 12:45:26 +0100

    elementos

--- DOM/06-crear-elementos/js/script.js
+++ DOM/06-crear-elementos/js/script.js
@@ -1,31 +1,38 @@
-//Rapida de escribir pero poco eficiente
-/*
-      <li>
-        Leche
-        <button class="remove-item btn-link text-red">
-          <i class="fa-solid fa-xmark"></i>
-        </button>
-      </li>
-*/
-
-//Traer una referencia al boton
-const btnAddItem = document.querySelector("#mi-boton");
-//Traer una referencia a la lista
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
 const lista = document.querySelector("#item-list");
 
+//Event Listeners
+itemForm.addEventListener("submit", (evt) => {
+  console.log(evt);
+  evt.preventDefault();
+  console.log("Añadir item!!!", itemInput.value);
+  const newItem = createListItem(itemInput.value);
+  lista.appendChild(newItem);
+});
+
+//Limpia y más rendimiento
 function createListItem(textItem) {
-  const li = document.createElement("li");
-  li.innerHTML = `${textItem}
-                  <button class="remove-item btn-link text-red">
-                          <i class="fa-solid fa-xmark"></i>
-                  </button>`;
-  lista.appendChild(li);
-  console.log(li);
+  const li = document.createElement("li"); //<li></li>
+  const text = document.createTextNode(textItem); //texto...
+  li.appendChild(text); //<li>texto...</li>
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  return li;
 }
 
-//Event Listeners
-btnAddItem.addEventListener("click", () => {
-  console.log("Añadir item!!!");
-  createListItem(itemInput.value);
-  itemInput.value = "";
-});
+function createButton(clases) {
+  const button = document.createElement("button");
+  button.className = clases;
+
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(clases) {
+  const icon = document.createElement("i");
+  icon.className = clases;
+  return icon;
+}
