f40dfc13b573e1a7935e80e3a6b552192017b062
Greissynv <greissynv@gmail.com>
2025-01-12 21:22:45 +0100

    final

--- DOM/07-eventos/js/script.js
+++ DOM/07-eventos/js/script.js
@@ -1,18 +1,26 @@
-/*
-<div class="key">a<small>e.key</small></div>
-<div class="key">65<small>e.keyCode</small></div>
-<div class="key">KeyA<small>e.code</small></div>
- */
-const insertDiv = document.querySelector("#insert");
-window.addEventListener("keyup", (e) => {
-  console.log(e);
-  insertDiv.innerHTML = `<div class="key">${e.key}
-                                <small>e.key</small>
-                        </div>
-                        <div class="key">${e.keyCode}
-                                <small>e.key</small>
-                        </div>
-                        <div class="key">${e.code}
-                                <small>e.key</small>
-                        </div>`;
-});
+function getKeyCodeInnerHTML(e) {
+        console.log(e);
+        const insertDiv = document.querySelector("#insert");
+        insertDiv.innerHTML = `<div class="key">${e.key === " " ? "Espacio" : e.key}
+                                        <small>e.key</small>
+                                </div>
+                                <div class="key">${e.keyCode}
+                                        <small>e.keyCode</small>
+                                </div>
+                                <div class="key">${e.code}
+                                        <small>e.code</small>
+                                </div>`;
+      }
+      function getKeyCodeCreateNodes(e) {
+        //TODO obtener la misma funcionalidad que la funcion anterior
+        //pero usando createElement, createTextNode y appendChild
+        //Sin innerHtml
+        const insertDiv = document.querySelector("#insert");
+        const div = document.createElement("div");
+        /**
+         * El codigo restante aqui....
+         */
+        insertDiv.appendChild(div);
+      }
+      // window.addEventListener("keyup", getKeyCodeInnerHTML);
+      window.addEventListener("keyup", getKeyCodeCreateNodes);
