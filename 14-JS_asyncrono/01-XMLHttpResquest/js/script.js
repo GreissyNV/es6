be4e2f7e8278955cb4b808f58b5451eaee891b90
Greissy Naranjo <greissynv@gmail.com>
2025-01-16 14:20:06 +0100

    pdt, copiar prof

--- 14-JS_asyncrono/01-XMLHttpResquest/js/script.js
+++ 14-JS_asyncrono/01-XMLHttpResquest/js/script.js
@@ -1,3 +1,37 @@
 //Pedir los 10 pokemos y listar sus nombres
 //https://pokeapi.co/api/v2/pokemon?limit=10
 //Parte del ejemplo de las pelis con XMLHTTPRequest
+
+function fetchPokemonData(){
+    const baseUrl = https://pokeapi.co/api/v2/pokemon?limit=10
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", baseUrl, true);
+}
+
+fetchPokemonData();
+
+
+//Asyn/await
+
+
+xhr.open("GET", "./pokemon.json"); 
+xhr.onreadystatechange = function () {
+console.log("Cambio de estado de la peticion");
+  console.log("Codigo estado: ", this.readyState);
+  if (this.readyState === 4 && this.status === 200) {
+    //convertimos a array para trabajar con los datos
+    const data = JSON.parse(this.responseText);
+    //insertar los datos en el HTML
+    data.forEach((pokemons) => {
+      const li = document.createElement("li");
+      li.innerHTML = `Name: ${pokemons.name}, url: ${pokemons.url}`;
+      document.querySelector("#pokemon").appendChild(li);
+    });
+    console.log(data);
+  }
+};
+
+xhr.send();
+
+console.log("Página cargada");
