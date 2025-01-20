d23aeacb0378d7df7ab620975e94448b0642ed52
Greissynv <greissynv@gmail.com>
2025-01-20 19:24:17 +0100

    final

--- 14-JS_asyncrono/01-XMLHttpResquest/js/script_json.js
+++ 14-JS_asyncrono/01-XMLHttpResquest/js/script_json.js
@@ -1,34 +1,32 @@
 const pelis = [
     {
-        title : "Piratas del caribe",
-        year: 2003,
+      title: "Piratas del caribe",
+      year: 2003,
     },
     {
-        title: "Titanic",
-        year: 1998,
+      title: "Titanic",
+      year: 1998,
     },
     {
-        title: "Hakers",
-        year: 1995,
+      title: "Hackers",
+      year: 1995,
     },
     {
-        title: "Avatar",
-        year: 2009,
-    }
-];
-console.log(pelis);
-const pelisJson = JSON.stringify(pelis);
-console.log(pelisJson); //Aqui esta en formato javascript
-
-let userJSON = '{"name": "John", "age": 30, "car":null}'; // es: propiedad : + valor
-const user = JSON.parse(userJSON);
-
-console.log(user);
-user.car ="BMW"
-console.log(user); //aqui esta en fromato JSON
-
-userJSON = JSON.stringify(user);
-console.log(userJSON); // Lo volvemos a pasar a Javascript
-
-
-
+      title: "Avatar",
+      year: 2009,
+    },
+  ];
+  console.log(pelis);
+  const pelisJSON = JSON.stringify(pelis);
+  console.log(pelisJSON);
+  
+  let userJSON = '{"name":"John", "age":30, "car":null}';
+  console.log(typeof userJSON);
+  
+  const user = JSON.parse(userJSON);
+  console.log(typeof user);
+  user.car = "BMW";
+  
+  console.log(user);
+  userJSON = JSON.stringify(user);
+  console.log(userJSON);
